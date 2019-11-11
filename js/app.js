4dc38016aa979d8756665859303e1d303a08dbd0
Yash Kadam <yashkadam278@gmail.com>
2019-11-11 15:23:34 +0530

    App : v1.0

--- js/app.js
+++ js/app.js
@@ -7,16 +7,19 @@ const navSlide = () => {
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-    //* =========== Animate Links
 
+    //* =========== Animate Links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          0.5}s`;
+          0.3}s`;
       }
     });
+
+    //* ============ Animate Burger
+    burger.classList.toggle("toggle");
   });
 };
 
